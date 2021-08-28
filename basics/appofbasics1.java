8b4aeb9647f60eeb33b125d63b479fec3280815e
clarrie-w <clarriewang@hotmail.com>
2021-08-28 09:29:57 -0400

    outline of lesson material for basics 1

---
+++ basics/appofbasics1.java
@@ -0,0 +1,23 @@
+public class appofbasics1 {
+    public static void main(String[] args) {
+
+        // Declaring variables: type name = value;
+        int x = 1;
+        int y = 2;
+        int z = 3;
+
+        int perimeter = x + y + z;
+        System.out.println(perimeter);
+
+        // Comparing side lengths x and y using if/else statements
+        if (x > y) {
+            System.out.println("Side x is longer.");
+        } else if (x == y) {
+            System.out.println("They are the same length.");
+        } else {
+            System.out.println("Side y is longer.");
+        }
+
+    }
+
+}
