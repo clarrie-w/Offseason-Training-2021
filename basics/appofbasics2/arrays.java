f3330b7f9701fa3251a0d4775709ab76e1856853
clarrie-w <clarriewang@hotmail.com>
2021-09-06 21:05:46 -0400

    arrays material added

---
+++ basics/appofbasics2/arrays.java
@@ -0,0 +1,16 @@
+package appofbasics2;
+
+public class arrays {
+    public static void main(String[] args){
+        String[] food = new String[4];
+        // String[] food = {"dumplings", "apple", "peanut butter", "oat milk"};
+        food[0] = "dumplings";
+        food[1] = "apple";
+        food[2] = "peanut butter";
+        food[3] = "oat milk";
+
+        for (int i = 0; i < food.length; i++) {
+            System.out.println(food);
+        }
+    }
+}
