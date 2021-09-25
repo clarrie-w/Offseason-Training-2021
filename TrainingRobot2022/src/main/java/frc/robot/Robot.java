a6c47c528253ed4d2ad02f5ed234ee61a0704b93
clarrie-w <clarriewang@hotmail.com>
2021-09-25 15:07:07 -0400

    added subsystems and commands

--- TrainingRobot2022/src/main/java/frc/robot/Robot.java
+++ TrainingRobot2022/src/main/java/frc/robot/Robot.java
@@ -4,21 +4,50 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+import frc.robot.commands.AutoScoreArm;
+import frc.robot.commands.DriveCommand;
+import frc.robot.commands.OI;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.LimelightSubsystem;
+import frc.robot.subsystems.TurretSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
+ * the package after creating this project, you must also update the manifest file in the resource
+ * directory.
  */
 public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  
+  private final DifferentialDrive m_robotDrive =
+      new DifferentialDrive(new PWMVictorSPX(0), new PWMVictorSPX(1));
+  private final Joystick m_stick = new Joystick(0);
+  private final Timer m_timer = new Timer();
+  public static OI OI = new OI(); // tells robot that it has a OI object
+  public static DriveSubsystem DriveSubsystem = new DriveSubsystem();
+  public static DriveCommand DriveCommand = new DriveCommand();
+  CommandScheduler commandScheduler = CommandScheduler.getInstance(); // returns command scheduler object
+  public static ArmSubsystem ArmSubsystem = new ArmSubsystem(1.0, 0.0, 0.0);
+  public static TurretSubsystem TurretSubsystem = new TurretSubsystem(1.0, 0.0, 0.0);
+  Command AutoCommand;
+  public static LimelightSubsystem LimelightSubsystem = new LimelightSubsystem();
+
+
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -29,6 +58,9 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+    
+    // m_timer.reset();
+    // m_timer.start();
   }
 
   /**
@@ -52,33 +84,35 @@ public class Robot extends TimedRobot {
    * chooser code above as well.
    */
   @Override
-  public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
+	public void autonomousInit() {
+    AutoCommand = new AutoScoreArm();
   }
-
+  
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
+    AutoCommand.start();
+    commandScheduler.run();
+
+    // Drive for 2 seconds
+    // if (m_timer.get() < 2.0) {
+    //   m_robotDrive.arcadeDrive(0.5, 0.0); // drive forwards half speed
+    // } else {
+    //   m_robotDrive.stopMotor(); // stop robot
+    // }
   }
 
-  /** This function is called once when teleop is enabled. */
+  /** This function is called once each time the robot enters teleoperated mode. */
   @Override
   public void teleopInit() {}
 
-  /** This function is called periodically during operator control. */
+  /** This function is called periodically during teleoperated mode. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+     // Start the CommandScheduler to add Command calls to the current running stack
+     commandScheduler.run();
+     DriveCommand.schedule();
+  }
 
   /** This function is called once when the robot is disabled. */
   @Override
@@ -88,7 +122,7 @@ public class Robot extends TimedRobot {
   @Override
   public void disabledPeriodic() {}
 
-  /** This function is called once when test mode is enabled. */
+  /** This function is called once each time the robot enters test mode. */
   @Override
   public void testInit() {}
 
