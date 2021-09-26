a12eded715b394976f13856f46363409d3ee132a
lily-s-23 <59377732+lily-s-23@users.noreply.github.com>
2021-09-26 15:57:42 -0400

    Create basics.java
    Created solutions for first basics practice questions.

---
+++ Practice_Questions/Solutions/basics.java
@@ -0,0 +1,17 @@
+public class basics{
+    public static void main(String[] args){
+        //Create a program that saves the height, width and length as three valuesv(integers).
+        int height = 12;
+        int width = 43;
+        int length = 21;
+        
+        //Then, compute the volume of this box and store it in a variable.
+        int volume = height * width * length;
+
+        //Print the volume of the box
+        System.out.println(volume);
+
+
+        
+}}
+
