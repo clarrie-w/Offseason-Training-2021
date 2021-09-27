136de1597bbdd0c593e4891c6f259642cac42b45
lily-s-23 <59377732+lily-s-23@users.noreply.github.com>
2021-09-27 11:56:34 -0400

    Added template for basics lesson

---
+++ basics/appofbasicstemplate.java
@@ -0,0 +1,39 @@
+public class appofbasics1 {
+    public static void main(String[] args) {
+
+        // Declaring variables: type name = value;
+        //Declare three variables named x,y,z. X should store the value 1, y should store the value 2, and z should store the value 3.
+
+        // Create a variable named perimeter that adds the three sides variables together
+
+
+        // Print out the perimeter of the triangle
+
+
+        /* Comparing side lengths x and y using if/else statements:
+            if (condition) {
+                do stuff
+            }
+        */
+        // If the side value of x is greater than y, then print "Side x is longer"
+
+
+        // If the values of x and y are the same, then print "They are the same length"
+
+
+        // If the values of x and y do not satisfy any of the prior conditions, print "Side y is longer"    
+
+        
+        // Using a built-in method
+
+        // Save name as a variable
+        
+
+        //Use length method to figure out the number of characters in your name, save as a variable named length
+        
+        
+        //Print out the length of your name
+        
+    }
+
+}
